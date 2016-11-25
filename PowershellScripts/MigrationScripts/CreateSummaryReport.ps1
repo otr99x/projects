2d3160b7311a3676aa7e638e56d7f197644e2976
Joe Cheung <Joe.Cheung@shaw.ca>
2016-11-24 17:38:57 -0700

    added reporting object

--- PowershellScripts/MigrationScripts/CreateSummaryReport.ps1
+++ PowershellScripts/MigrationScripts/CreateSummaryReport.ps1
@@ -256,6 +256,40 @@ function Run-Report ($summaryinputfolder, $sharepointwebappurl)
     $SPContract | Export-Csv -Path "SummaryOutputFiles\SPContract.csv" -NoTypeInformation
     $SPPO | Export-Csv -Path "SummaryOutputFiles\SPPO.csv" -NoTypeInformation
     $SPRFI | Export-Csv -Path "SummaryOutputFiles\SPRFI.csv" -NoTypeInformation
+
+    # create the reporting object
+    $Report = New-Object -TypeName psobject
+    $Report | Add-Member -MemberType NoteProperty -Name 'ContractJDETotal' -Value 0      
+    $Report | Add-Member -MemberType NoteProperty -Name 'ContractJDEMigratable' -Value 0      
+    $Report | Add-Member -MemberType NoteProperty -Name 'ContractJDEPercent' -Value 0      
+    $Report | Add-Member -MemberType NoteProperty -Name 'ContractSPRecordsMigrated' -Value 0      
+    $Report | Add-Member -MemberType NoteProperty -Name 'ContractSPFilesMigrated' -Value 0      
+    $Report | Add-Member -MemberType NoteProperty -Name 'ContractRecordsInCS' -Value 0      
+    $Report | Add-Member -MemberType NoteProperty -Name 'ContractFilesInCS' -Value 0      
+    $Report | Add-Member -MemberType NoteProperty -Name 'ContractRecordsInCSNotInSP' -Value 0      
+    $Report | Add-Member -MemberType NoteProperty -Name 'ContractItemsInSPNotInCS' -Value 0      
+    $Report | Add-Member -MemberType NoteProperty -Name 'ContractItemsInCSNotInSP' -Value 0      
+    $Report | Add-Member -MemberType NoteProperty -Name 'ContractURLInCS' -Value 0      
+    $Report | Add-Member -MemberType NoteProperty -Name 'ContractMigratedURLInSP' -Value 0      
+    $Report | Add-Member -MemberType NoteProperty -Name 'ContractMigratedContractFilePercent' -Value 0      
+    $Report | Add-Member -MemberType NoteProperty -Name 'ContractMigratedContractRecordPercent' -Value 0      
+
+    $Report.ContractJDETotal = @($JDEContract).count
+    $Report.ContractJDEMigratable= @($JDEContract | Where-Object -Property Path -Like ':Enterprise:Upstream Operations:Upstream Business Services:Supply Management:JDE Attachments:SCM-CCA-Contracts*').count
+    $Report.ContractJDEPercent = "{0:p2}" -f ($Report.ContractJDEMigratable / $Report.ContractJDETotal)
+    $Report.ContractSPRecordsMigrated = @($SPContract | Where-Object -Property 'Content Type' -EQ 'Contracts Document Set' | Where-Object -Property 'Livelink ID' -GT 0).Count
+    $Report.ContractSPFilesMigrated = @($SPContract | Where-Object -Property 'Content Type' -EQ 'Contracts' | Where-Object -Property 'LiveLink ID' -GT 0).Count + @($SPContract | Where-Object -Property 'Content Type' -EQ 'Document' | Where-Object -Property 'LiveLink ID' -GT 0).Count
+    $Report.ContractRecordsInCS = @($CSContract | Where-Object -Property Path -EQ ':Enterprise:Upstream Operations:Upstream Business Services:Supply Management:JDE Attachments:SCM-CCA-Contracts').count
+    $Report.ContractFilesInCS = @($CSContract | Where-Object -Property Subtype -EQ 'Document').Count + @($CSContract | Where-Object -Property Subtype -EQ '' | Where-Object -Property DATAID -GT 0).Count
+    $Report.ContractRecordsInCSNotInSP = @($CSContract | Where-Object -Property sp-dataid-lookup -EQ '' | Where-Object -Property Subtype -EQ 'Folder' | Where-Object -Property Path -EQ ':Enterprise:Upstream Operations:Upstream Business Services:Supply Management:JDE Attachments:SCM-CCA-Contracts').Count
+    $Report.ContractItemsInSPNotInCS = @($SPContract | Where-Object -Property cs-dataid-lookup -EQ '' | Where-Object -Property 'Item Type' -NE 'Folder' | Where-Object -Property 'Item Type' -NE 'URL' | Where-Object -Property DATAID -GT 0).Count
+    $Report.ContractItemsInCSNotInSP = @($CSContract | Where-Object -Property sp-dataid-lookup -EQ '' | Where-Object -Property Subtype -EQ 'Document').Count + @($CSContract | Where-Object -Property sp-dataid-lookup -EQ '' | Where-Object -Property Subtype -EQ '').Count
+    $Report.ContractURLInCS = @($CSContract | Where-Object -Property Subtype -EQ 'URL').Count
+    $Report.ContractMigratedURLInSP = @($SPContract | Where-Object -Property 'Content Type' -EQ 'Link to a Document').Count
+    $Report.ContractMigratedContractFilePercent = "{0:p2}" -f ($Report.ContractSPFilesMigrated / $Report.ContractFilesInCS)
+    $Report.ContractMigratedContractRecordPercent = "{0:p2}" -f ($Report.ContractSPRecordsMigrated / $Report.ContractJDEMigratable)
+    $Report
+
 }
 
 Run-Report -summaryinputfolder 'SummaryInputFiles' -sharepointwebappurl 'https://teamsiteppd.cenovus.com/'
